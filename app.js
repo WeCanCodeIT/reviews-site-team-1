d6f6d9f9cdf59786c48625201558f438ef0c2810
Omar Jadan <o.jadan197@gmail.com>
2019-10-10 14:21:24 -0400

    Commiting new work

--- app.js
+++ app.js
@@ -23,8 +23,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/tables', categoryRouter);
-app.use('/reviews', review)
+// app.use('/tables', categoryRouter);
+// app.use('/reviews', review)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
