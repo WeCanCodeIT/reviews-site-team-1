41abba86f4b0fd5b5317d53ed58763dcb0fbfdff
Richard Wilson <rcwilson88@gmail.com>
2019-10-10 11:39:02 -0400

    added Reviews page, Services, and updated Controller

---
+++ controllers/review.js
@@ -0,0 +1,25 @@
+const ReviewDomainObject = require("../Model/Review");
+const reviewService = require("../services/review-service");
+
+
+class ReviewController{
+
+    static newReview(req, res){
+        const title = req.body.title;
+        const content= req.body.content;
+        const category = req.body.category;
+        const rating = req.body.rating;
+        const tags = req.body.tags;
+        
+        reviewService.addReview(new ReviewDomainObject(title, content, rating, category, tags), (createdReview) => {
+        res.redirect("/reviews")}
+        )}   
+
+    static getReviews(req, res){
+        reviewService.findAll((reviews) => {
+        res.render("reviews", { reviews });
+        })
+    };
+}
+
+module.exports = ReviewController;
