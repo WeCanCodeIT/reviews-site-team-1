d061d9229f960d0a713742282530467ea7bf1ea0
Derrick White <derrickpwhite1@gmail.com>
2019-10-11 14:36:33 -0400

    replaced callback function in category with async/await, added categories drop menu

--- services/category-service.js
+++ services/category-service.js
@@ -3,13 +3,19 @@ const Category = require("../utils/category");
 
 module.exports = {
 
-    findAll (callback) {
-        Category.findAll().then(callback)
+    // findAll (callback) {
+    //     Category.findAll().then(callback)
+    // },
+
+    async findAll() {
+        return await Category.findAll()
     },
 
-    findById (id, callback) {
-        Category.findByPk(id).then(callback)
-        }
-    };
+    // findById (id, callback) {
+    //     Category.findByPk(id).then(callback)
+    //     },
 
-    //
+    async findById (id) {
+        return await Category.findByPk(id)
+    }
+    };
