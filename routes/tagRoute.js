4ee65b4dfa421a62e0a58295728ffccd39be2b7b
Richard Wilson <rcwilson88@gmail.com>
2019-10-16 15:53:05 -0400

    add tag and tag-view feature complete

--- routes/tagRoute.js
+++ routes/tagRoute.js
@@ -1,13 +1,12 @@
 const  express = require('express');
 const router = express.Router();
+
 const TagController = require("../controllers/tagController");
 
 
 router.get("/", TagController.displayTags);
 
-
-
-
+router.post("/", TagController.newTag);
 
 
 
--- services/tag-service.js
+++ services/tag-service.js
@@ -2,11 +2,14 @@ const Tags = require("../utils/tag");
 
 module.exports = {
     async findAll () {
-
         return await Tags.findAll()
     },
 
     async findById (id) {
         return await Tags.findByPk(id)
-        }
+    },
+
+    async addTag (tag) {
+        await Tags.create(tag);
+    }
     };
